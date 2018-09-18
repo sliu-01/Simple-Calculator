1668ff25ca1979a5eb44cbeef7a2dd9796cce9ba
stevenliu-sl <stevenliu.sl01@gmail.com>
2018-09-18 00:03:08 -0400

    added subtraction method to calculator

--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -6,6 +6,9 @@ public class CalculatorMain
 	{
 		Calculator calc = new Calculator();
 		System.out.println(calc.add(2,4));
+		
+		CalculatorSubtract calc2 = new CalculatorSubtract();
+		System.out.println(calc2.subtract(10,6));
 	}
 
 }
---
+++ src/CalculatorSubtract.java
@@ -0,0 +1,10 @@
+
+public class CalculatorSubtract 
+{
+	public int subtract(int num1, int num2)
+	{
+		int ans = num1 - num2;
+		return ans;
+	}
+
+}
